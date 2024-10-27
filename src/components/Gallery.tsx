a66a9a39ee064657544c9ceb1faeb801e2737f0f
shtanriverdi <shtanriverdi@yahoo.com>
2024-10-27 12:33:09 +0300

    Cloudinary

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -4,14 +4,16 @@ import { autoGravity } from "@cloudinary/url-gen/qualifiers/gravity";
 import { AdvancedImage } from "@cloudinary/react";
 
 const App = () => {
-  const cld = new Cloudinary({ cloud: { cloudName: "dplejuooh" } });
+  const cld = new Cloudinary({
+    cloud: { cloudName: import.meta.env.VITE_CLOUDINARY_NAME },
+  });
 
   // Use this sample image or upload your own via the Media Explorer
   const img = cld
-    .image("cld-sample-5")
+    .image("gullu_ecommerce/q0parejvgazxyj3pmnme")
     .format("auto") // Optimize delivery by resizing and applying auto-format and auto-quality
     .quality("auto")
-    .resize(auto().gravity(autoGravity()).width(500).height(500)); // Transform the image: auto-crop to square aspect_ratio
+    .resize(auto().gravity(autoGravity()).width(200).height(200)); // Transform the image: auto-crop to square aspect_ratio
 
   return <AdvancedImage cldImg={img} />;
 };
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,9 +1,10 @@
 import Product from "../supabase/model/Product";
+import Gallery from "../components/Gallery";
 
 export default function ProductCard({ title, desc, price, img_url }: Product) {
   return (
-    <div className="w-min-[20rem] h-min-[20rem] bg-slate-300">
-      {/* <img src={img_url} alt={title} className="w-full h-48 object-cover" /> */}
+    <div className="w-[20rem] h-[20rem] bg-slate-300">
+      <Gallery />
       <h2 className="text-xl font-bold">{title}</h2>
       <p className="text-gray-600">{desc}</p>
       <p className="text-green-500 font-semibold">${price}</p>
