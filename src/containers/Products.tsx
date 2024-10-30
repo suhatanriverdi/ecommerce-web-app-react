80007dc2eb4db3d7b533dce111af0b470329fb19
shtanriverdi <shtanriverdi@yahoo.com>
2024-10-30 16:44:29 +0300

    Implement jotai for products

--- src/containers/Products.tsx
+++ src/containers/Products.tsx
@@ -5,24 +5,101 @@ import ProductCard from "../components/ProductCard";
 import useSWR from "swr";
 import NotFound from "../pages/NotFound";
 import { fetcher } from "../supabase/utils/fetcher";
+import { useRef, useEffect, useState } from "react";
+import { debounce } from "lodash";
+import { SupabaseClient } from "@supabase/supabase-js";
+import { atom, useAtom } from "jotai";
+
+// Products Atom
+export const productsAtom = atom<Product[]>([]);
 
 export default function Products() {
+  // Jotai State Management
+  const [products, setProducts] = useAtom(productsAtom);
+
+  // Scrolling
+  const PAGE_COUNT = 10;
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const [offset, setOffset] = useState(1);
+  const [isScrollProductsLoading, setIsScrollProductsLoading] = useState(false);
+  const [isInView, setIsInView] = useState(false);
+
+  const handleScroll = () => {
+    if (containerRef.current && typeof window !== "undefined") {
+      const container = containerRef.current;
+      const { bottom } = container.getBoundingClientRect();
+      const { innerHeight } = window;
+      setIsInView((prevIsInView) => bottom <= innerHeight);
+    }
+  };
+
+  const loadMoreProducts = async (offset: number) => {
+    setIsScrollProductsLoading(true);
+    // Every time we fetch, we want to increase
+    // the offset to load fresh Products
+    setOffset((prev) => prev + 1);
+    const { data: newProducts } = await fetchProducts(offset, PAGE_COUNT);
+    // Merge new Products with all previously loaded
+    setLoadedProducts((prevProducts) => [...prevProducts, ...newProducts]);
+    setIsScrollProductsLoading(false);
+  };
+
+  // Infinite Scroll, to load more
+  useEffect(() => {
+    if (isInView) {
+      loadMoreProducts(offset);
+    }
+  }, [isInView]);
+
+  // Infinite Scroll
+  useEffect(() => {
+    const handleDebouncedScroll = debounce(() => handleScroll(), 200);
+    window.addEventListener("scroll", handleDebouncedScroll);
+    return () => {
+      window.removeEventListener("scroll", handleDebouncedScroll);
+    };
+  }, []);
+
+  const fetchProducts = async (
+    offset: number,
+    limit: number,
+    supabase: SupabaseClient
+  ) => {
+    const from = offset * PAGE_COUNT;
+    const to = from + PAGE_COUNT - 1;
+
+    const { data } = await supabase
+      .from("products")
+      .select("*")
+      .range(from, to);
+
+    return data;
+  };
+
   // Use the SWR hook for data fetching
   const supabase = useSupabase();
-  const {
-    data: products,
-    error,
-    isLoading,
-  } = useSWR("products", () => fetcher(supabase));
+  const { data, error, isLoading } = useSWR("products", () =>
+    fetcher(supabase)
+  );
 
-  // TODO
-  if (error) {
-    return <NotFound />;
+  // Update the products atom right after SWR fetches
+  if (data) {
+    setProducts(data);
   }
 
   // TODO
+  // Loading state, this is needed otherwise will produce an error
   if (isLoading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="w-full max-w-[62rem] mt-[130px] flex flex-col items-center justify-center">
+        Loading...
+      </div>
+    );
+  }
+
+  // TODO
+  if (error) {
+    return <NotFound reason={"supabase"} />;
   }
 
   // Build the cards
@@ -46,7 +123,10 @@ export default function Products() {
           <p>Erkek → Tüm Ürünler</p>
           <p>Sırala</p>
         </div>
-        <div className="grid grid-cols-[repeat(auto-fit,minmax(12rem,1fr))] w-full gap-[1rem] mt-[1rem]">
+        <div
+          ref={containerRef}
+          className="grid grid-cols-[repeat(auto-fit,minmax(12rem,1fr))] w-full gap-[1rem] mt-[1rem]"
+        >
           {productCards}
         </div>
       </motion.div>
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,9 +1,15 @@
 import { Link } from "react-router-dom";
 
-export default function NotFound() {
+type Reason = {
+  reason?: string | null;
+};
+
+export default function NotFound({ reason }: Reason) {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <p className="text-6xl">URL Not Found :(</p>
+      <p className="text-6xl">
+        {reason === "supabase" ? "Supabase Error" : "URL Not Found :("}
+      </p>
       <Link
         className="p-5 mt-10 bg-blue-200 border cursor-pointer border-x-sky-200 rounded-2xl"
         to="/"
