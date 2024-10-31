d47591e993702a3adad87ae059e11ecd2f65f7ac
shtanriverdi <shtanriverdi@yahoo.com>
2024-10-31 14:37:27 +0300

    Start implementation for Sorting and URL Breadcrumb mechanism

--- src/components/CategoriesSortNavBar.tsx
+++ src/components/CategoriesSortNavBar.tsx
@@ -1,8 +1,14 @@
+import { Link, useParams } from "react-router-dom";
+
 export default function CategoriesSortNavBar() {
+  const { gender } = useParams();
+  console.log("path:", gender);
+  const currentCategory: string = gender === "men" ? "Erkek" : "Kadın";
+
   return (
     <div className="text-lg flex w-full max-w-[62rem] justify-between items-center text-center pt-[1rem]">
-      <p>Erkek → Tüm Ürünler</p>
-      <p>Sırala</p>
+      <p>{currentCategory} → Tüm Ürünler</p>
+      <Link to={`?sort_by=`}>Sırala</Link>
     </div>
   );
 }
