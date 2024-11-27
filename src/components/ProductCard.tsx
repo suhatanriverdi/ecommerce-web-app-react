2be66e3514724cb97a172b97a32ee413a990d33f
shtanriverdi <shtanriverdi@yahoo.com>
2024-11-27 09:16:18 +0300

    Fix Loading

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -2,23 +2,23 @@ import { useState } from "react";
 import ClodinaryImg from "./ClodinaryImg";
 import Product from "../supabase/model/Product.ts";
 
-export default function ProductCard({ title, desc, price, img_url }: Product) {
+export default function ProductCard({ title, price, img_url }: Product) {
   const [isLoading, setIsLoading] = useState(true);
 
   return (
-    <div className="grid justify-items-start hover:shadow-[0px_0px_4px_1px_rgba(0,_120,_90,_0.4)] cursor-pointer content-between shadow-[0px_0px_3px_1px_rgba(0,_0,_0,_0.1)] p-4">
+    <div className="flex flex-col h-[30rem] p-6 justify-between items-center hover:shadow-[0px_0px_4px_1px_rgba(0,_120,_90,_0.4)] cursor-pointer shadow-[0px_0px_3px_1px_rgba(0,_0,_0,_0.1)]">
+      {!isLoading && <h2 className="text-lg">{title}</h2>}
       <ClodinaryImg
         img_url={img_url}
         isLoading={isLoading}
         setIsLoading={setIsLoading}
       />
       {!isLoading && (
-        <>
-          <h2 className="font-bold text-xl">{title}</h2>
-          <p className="text-green-500 text-3xl justify-self-center">
+        <div className="flex flex-col justify-center items-center">
+          <p className="text-button-bg-dark text-4xl">
             {price} <span className="font-extralight">₺</span>
           </p>
-        </>
+        </div>
       )}
     </div>
   );
