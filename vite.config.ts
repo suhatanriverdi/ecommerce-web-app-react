5242681bc8e01a2fe82c42ab84878c1b22b96a09
shtanriverdi <shtanriverdi@yahoo.com>
2024-12-19 00:31:12 +0300

    Fix Vercel Path

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
-  build: {
-    outDir: "dist",
+  base: process.env.VITE_BASE_PATH,
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
